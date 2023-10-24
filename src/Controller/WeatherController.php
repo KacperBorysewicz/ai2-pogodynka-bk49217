e406b527c5b7a861b8ca6df355ad7ca259eb33c1
bk49217 <bk49217@zut.edu.pl>
2023-10-24 21:13:28 +0200

    Commit after lab C

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MeasurementRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Location;
+use App\Entity\Measurement;
+
+class WeatherController extends AbstractController
+{
+
+    #[Route('/weather/{city}', name: 'app_weather', requirements: ["id" => "\d+"])]
+    #[Route('/weather/{city}/{country}', name: 'app_weather_with_country', requirements: ["id" => "\d+"])]
+
+    public function city(Location $location, MeasurementRepository $repository): Response
+    {
+        $measurements = $repository->findByLocation($location);
+        return $this->render('weather/city.html.twig', [
+            'location' => $location,
+            'measurements' => $measurements,
+        ]);
+    }
+}
